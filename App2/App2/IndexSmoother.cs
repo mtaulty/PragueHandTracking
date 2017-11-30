0c3038f389e59540205a620c99602cf2a26a044c
Mike Taulty <mtaulty@microsoft.com>
2017-11-30 14:41:46 +0000

    Initial Commit

---
+++ App2/App2/IndexSmoother.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Gestures.Skeleton;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Gestures.Samples.Camera3D
+{
+    public class SmoothedPositionChangeEventArgs : EventArgs
+    {
+        /// <summary> Position of palm after jitter was smoothed. </summary>
+        public Vector3 SmoothedPosition { get; set; }
+        /// <summary> Difference between current smoothed position and previous one. </summary>
+        public Vector3 SmoothedPositionDelta { get; set; }
+    }
+
+    // Note - I borrowed this class from the PalmSmoother class in the Gestures samples
+    // simply making it work off the index finger position rather than the palm position.
+    // It might be a case for using something like ReactiveExtensions rather than writing
+    // this whole class here but it was available so I borrowed it.
+    public class IndexSmoother
+    {
+        const int DefaultWindowSize = 5; // [samples]
+        const float DefaultJumpThreshold = 20; // [mm]        
+
+        private readonly int _windowSize;
+        private readonly float _jumpThreshold;
+
+        private Queue<Vector3> _window;
+        private Vector3 CurrentAverage => _window.Any() ? _window.Aggregate((v1, v2) => v1 + v2) * (1f / _window.Count) : new Vector3(0, 0, 0);
+
+        public event EventHandler<SmoothedPositionChangeEventArgs> SmoothedPositionChanged;
+
+        public IndexSmoother(int windowSize = DefaultWindowSize, float jumpThreshold = DefaultJumpThreshold)
+        {
+            _window = new Queue<Vector3>(windowSize);
+            _windowSize = windowSize;
+            _jumpThreshold = jumpThreshold;
+        }
+
+        public void Smooth(IHandSkeleton skeleton)
+        {
+            var indexPosition = skeleton.IndexPosition;
+            var previousAverage = CurrentAverage;
+
+            if ((indexPosition - previousAverage).TwoNorm() > _jumpThreshold)
+            {
+                // filter jump - flush the queue and start over averaging
+                _window.Clear();
+                _window.Enqueue(indexPosition);
+                return;
+            }
+
+            _window.Enqueue(indexPosition);
+            if (_window.Count > _windowSize)
+            {
+                _window.Dequeue();
+            }
+
+            var currentAverage = CurrentAverage;
+
+            SmoothedPositionChanged?.Invoke(this, new SmoothedPositionChangeEventArgs()
+            {
+                SmoothedPosition = currentAverage,
+                SmoothedPositionDelta = currentAverage - previousAverage
+            });
+        }
+
+    }
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Gestures;
+using Microsoft.Gestures.Endpoint;
+using Microsoft.Gestures.Samples.Camera3D;
+using System;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+        async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            this.gestureService = GesturesServiceEndpointFactory.Create();
+            await this.gestureService.ConnectAsync();
+
+            this.smoother = new IndexSmoother();
+            this.smoother.SmoothedPositionChanged += OnSmoothedPositionChanged;
+
+            await this.gestureService.RegisterToSkeleton(this.OnSkeletonDataReceived);
+        }
+        void CanvasSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.canvasSize = e.NewSize;
+        }
+        void OnSkeletonDataReceived(object sender, HandSkeletonsReadyEventArgs e)
+        {
+            var right = e.HandSkeletons.FirstOrDefault(h => h.Handedness == Hand.RightHand);
+
+            if (right != null)
+            {
+                this.smoother.Smooth(right);
+            }
+        }
+        async void OnSmoothedPositionChanged(object sender, SmoothedPositionChangeEventArgs e)
+        {
+            // AFAIK, the positions here are defined in terms of millimetres and range
+            // -ve to +ve with 0 at the centre.
+
+            // I'm unsure what range the different cameras have in terms of X,Y,Z and
+            // so I've made up my own range which is X from -200 to 200 and Y from
+            // -90 to 90 and that seems to let me get "full scale" on my hand 
+            // movements.
+
+            // I'm sure there's a better way. X is also reversed for my needs so I
+            // went with a * -1.
+
+            var xPos = Math.Clamp(e.SmoothedPosition.X * - 1.0, 0 - XRANGE, XRANGE);
+            var yPos = Math.Clamp(e.SmoothedPosition.Y, 0 - YRANGE, YRANGE);
+            xPos = (xPos + XRANGE) / (2.0d * XRANGE);
+            yPos = (yPos + YRANGE) / (2.0d * YRANGE);
+
+            await this.Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    this.marker.Visibility = Visibility.Visible;
+
+                    var left = (xPos * this.canvasSize.Width);
+                    var top = (yPos * this.canvasSize.Height);
+
+                    Canvas.SetLeft(this.marker, left - (this.marker.Width / 2.0));
+                    Canvas.SetTop(this.marker, top - (this.marker.Height / 2.0));
+                    this.txtDebug.Text = $"{left:N1},{top:N1}";
+                }
+
+            );
+        }
+        static readonly double XRANGE = 200;
+        static readonly double YRANGE = 90;
+        Size canvasSize;
+        GesturesServiceEndpoint gestureService;
+        IndexSmoother smoother;
+    }
+}
